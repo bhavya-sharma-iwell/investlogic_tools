5854efb957d86f2c6e2243e1f79c38f769cef221
MohitYadav-Investwell <mohit.yadav@investwellonline.com>
2023-06-13 16:53:06 +0530

    fix all api

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyparser.json()) 
 
-app.use('/*', require('./routes/portfolioCorrelation'))    
-app.use('/invoiceGenerator', require('./routes/invoiceGenerator'))
+app.use('/api/portfolioCorrelation', require('./routes/portfolioCorrelation'))
+app.use('/api/invoiceGenerator', require('./routes/invoiceGenerator'))
 app.use('/api/portfolioOverlap', require('./routes/portfolioOverlap'))
 
-// app.all('/*', (req, res) => {
-//   res.status(404).send('API cannot be  Found')
-// })
+app.all('/*', (req, res) => {
+  res.status(404).send('API cannot be  Found')
+})
 
 //running the server
 app.listen(3000, () => { console.log(`Express server listening on port 3000`) })
